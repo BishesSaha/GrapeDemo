b1700a8be51ca8d21036bfeeaff5358a63d0d326
BishesSaha <bishes@sflhub.com>
2022-08-19 14:17:11 +0530

    Grape Files Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WebBuilder from './WebBuilder'
+
+function App() {
+  return (
+      <div id='APP'>
+          <WebBuilder />
+      </div>
+  )
+}
+
+export default App;
